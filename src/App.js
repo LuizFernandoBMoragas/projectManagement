ed676861299c365e42083aa2c52733fb1e28aaeb
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-05-06 20:36:13 +0100

    adding axios and first imputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+// import Axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <imput className="name" />
+    </div>
+  );
 }
 
 export default App;

7dd0a0c6747c696b848f9df6be5b7b2ad6700b0a
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-05-07 22:08:23 +0100

    adding all other inputs and buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <label>Project Name: </label>
+      <imput className="name" />
+      <label>Start date: </label>
+      <imput className="initial-date" />
+      <label>End project: </label>
+      <imput className="end-date" />
+      <label>Name: </label>
+      <imput className="name" />
+      <label>Name: </label>
+      <imput className="name" />
+      <label>Name: </label>
+      <imput className="name" />
+      <label>Name: </label>
       <imput className="name" />
     </div>
   );
